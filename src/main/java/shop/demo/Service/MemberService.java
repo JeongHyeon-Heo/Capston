10558613e0ba6576795c84e27ea12392fba38837
JeongHYeon-Heo <auspiciousjh@hansung.ac.kr>
2024-04-11 20:47:49 +0900

    MemberService, MemberRepository

---
+++ src/main/java/shop/demo/Service/MemberService.java
@@ -0,0 +1,33 @@
+package shop.demo.Service;
+
+import org.springframework.stereotype.Service;
+import shop.demo.Repository.MemberRepository;
+import shop.demo.domain.Member;
+
+import java.util.List;
+
+@Service
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void saveMember(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Member findMemberById(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public List<Member> findMembersByName(String name) {
+        return memberRepository.findByName(name);
+    }
+}
