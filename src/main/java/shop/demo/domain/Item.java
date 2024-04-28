94f98b06879ea5ec38d36e7ee41c0ac0e04af1cd
hwakyungkwon <hkkwon04@gmail.com>
2024-04-28 20:01:47 +0900

    item repository, service 수정 itemController 추가

--- src/main/java/shop/demo/domain/Item.java
+++ src/main/java/shop/demo/domain/Item.java
@@ -1,27 +1,32 @@
 package shop.demo.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Getter @Setter
 public class Item {
     @Id @GeneratedValue
     @Column(name = "item_id")
-    private Long id;
+    private Long id; // 아이템 고유번호
 
-    private String name;
+    private String name; // 상품이름
+
+    private int price; // 가격
+
+    private int stockQuantity; //재고량
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
-
-    private int price;
-    private int stockQuantity;
 
 
     public void addStock(int quantity){
@@ -32,6 +37,4 @@ public class Item {
         int restStock = this.stockQuantity - quantity;
         this.stockQuantity = restStock;
     }
-
-
 }
